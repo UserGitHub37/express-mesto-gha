093cb0ecbc102251e2bf60e551377e93fbfa4df3
UserGitHub37 <bum37@mail.ru>
2022-07-26 22:13:39 +0300

    feat: create schemas and models for card and user

---
+++ models/card.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  }],
+  createdAt: {
+    type: Date,
+  },
+});
+
+module.exports = mongoose.model('card', cardSchema);
