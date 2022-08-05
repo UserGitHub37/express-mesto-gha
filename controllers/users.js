9f9cfcbd2accf97c629d9208cacfa6b5b147a497
UserGitHub37 <bum37@mail.ru>
2022-08-05 13:52:31 +0300

    feat: add password hashing and default values

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 const {
@@ -39,13 +40,14 @@ module.exports.createUser = (req, res) => {
     password,
   } = req.body;
 
-  User.create({
-    name,
-    about,
-    avatar,
-    email,
-    password,
-  })
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      name,
+      about,
+      avatar,
+      email,
+      password: hash,
+    }))
     .then((user) => res.status(STATUS_CODE_CREATED).send(user))
     .catch((err) => {
       if (err.name === 'ValidationError') {
