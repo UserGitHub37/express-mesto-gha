d20382a61ff9bd48a9e9c36e70b92902a0121956
UserGitHub37 <bum37@mail.ru>
2022-08-10 15:16:20 +0300

    feat: request validation for getUserById

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+
 const {
   getUsers,
   getUserById,
@@ -9,7 +11,11 @@ const {
 
 router.get('/', getUsers);
 router.get('/me', getUser);
-router.get('/:userId', getUserById);
+router.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().length(24).hex(),
+  }),
+}), getUserById);
 router.patch('/me', updateUser);
 router.patch('/me/avatar', updateAvatar);
 
