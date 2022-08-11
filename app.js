daec2cfb33e3313a0d53a514bb0794a26b51f993
UserGitHub37 <bum37@mail.ru>
2022-08-12 01:11:29 +0300

    refactor: code refactoring

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const rateLimit = require('express-rate-limit');
 const cookieParser = require('cookie-parser');
 
 const { PORT = 3000 } = process.env;
-const { STATUS_CODE_NOT_FOUND, STATUS_CODE_INTERNAL_SERVER_ERROR } = require('./utils/statusCodes');
+const NotFoundError = require('./errors/not-found-err');
+const { STATUS_CODE_INTERNAL_SERVER_ERROR } = require('./utils/statusCodes');
 const { login } = require('./controllers/login');
 const { createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
@@ -30,12 +31,6 @@ app.use(cookieParser());
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(express.json());
-
-app.use((req, res, next) => {
-  // eslint-disable-next-line no-console
-  console.log(`${req.method}: ${req.path} ${JSON.stringify(req.body)}`);
-  next();
-});
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
@@ -60,7 +55,7 @@ app.use('/users', require('./routes/users'));
 
 app.use('/cards', require('./routes/cards'));
 
-app.use('*', (req, res) => res.status(STATUS_CODE_NOT_FOUND).send({ message: '404 Not Found' }));
+app.use('*', (req, res, next) => next(new NotFoundError('404 Not Found')));
 
 app.use(errors());
 
